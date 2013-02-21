74e6cd112d48ebf1b963948d966452a0dab7247b
Tkachenko Andrey <profuel@gmail.com>
2013-02-21 23:49:14 +0200

    - init commit for BLOG instance

---
+++ apps/blog/models.py
@@ -0,0 +1,11 @@
+from django.conf import settings
+from django.db import models
+
+class Blog(models.Model):
+	alias = models.CharField(unique = True, max_length = 32, null = False, blank = False)
+	active = models.BooleanField(default = True)
+
+class Post(models.Model):
+	blog = models.ForeignKey(Blog)
+	message = models.TextField(max_length = 1024)
+	image = models.ImageField(upload_to = settings.UPLOAD_DIR)
