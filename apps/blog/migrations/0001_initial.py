33b384bcf592fdea84010979e5da964f65ec2311
Tkachenko Andrey <profuel@gmail.com>
2013-02-21 23:49:37 +0200

    migration & DB created

---
+++ apps/blog/migrations/0001_initial.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'Blog'
+        db.create_table('blog_blog', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('alias', self.gf('django.db.models.fields.CharField')(unique=True, max_length=32)),
+            ('active', self.gf('django.db.models.fields.BooleanField')(default=True)),
+        ))
+        db.send_create_signal('blog', ['Blog'])
+
+        # Adding model 'Post'
+        db.create_table('blog_post', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('blog', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['blog.Blog'])),
+            ('message', self.gf('django.db.models.fields.TextField')(max_length=1024)),
+            ('image', self.gf('django.db.models.fields.files.ImageField')(max_length=100)),
+        ))
+        db.send_create_signal('blog', ['Post'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'Blog'
+        db.delete_table('blog_blog')
+
+        # Deleting model 'Post'
+        db.delete_table('blog_post')
+
+
+    models = {
+        'blog.blog': {
+            'Meta': {'object_name': 'Blog'},
+            'active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'alias': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '32'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'})
+        },
+        'blog.post': {
+            'Meta': {'object_name': 'Post'},
+            'blog': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['blog.Blog']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'}),
+            'message': ('django.db.models.fields.TextField', [], {'max_length': '1024'})
+        }
+    }
+
+    complete_apps = ['blog']
