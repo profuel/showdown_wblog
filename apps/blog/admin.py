6455d6e618e5371778274827aab79aa4942b43df
Tkachenko Andrey <profuel@gmail.com>
2013-02-22 02:06:46 +0200

    renamed admin views added default management for blog & posts

---
+++ apps/blog/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from blog.models import Blog, Post
+from apps.blog.admin_forms import PostAdminForm
+
+class PostInlineAdmin(admin.StackedInline):
+    model = Post
+    extra = 1
+    form = PostAdminForm
+
+class BlogAdmin(admin.ModelAdmin):
+    inlines = [PostInlineAdmin]
+
+
+admin.site.register(Blog, BlogAdmin)
---
+++ apps/blog/admin_forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from blog.models import Post
+from django.core.exceptions import ValidationError
+
+class PostAdminForm(forms.ModelForm):
+    max_single_message = 1024
+    max_message_with_image = 256
+    def clean_message(self):
+        import pdb; pdb.set_trace()
+        if self.cleaned_data.get('image'):
+            if len(self.cleaned_data['message']) > self.max_message_with_image:
+                raise ValidationError('Message with picture cannot exceed %s symbols!' % self.max_message_with_image)
+        elif len(self.cleaned_data['message']) > self.max_single_message:
+            raise ValidationError('Plain message cannot exceed %s symbols!' % self.max_single_message)
+        return self.cleaned_data['message']
+
+    class Meta:
+        model = Post
